d318aebab7ac19d55638d4784526a374ce2ecc95
João Gabriel Mauricio <joao.gabriel@builtcode.com.br>
2020-03-04 21:50:14 -0300

    Adicionado Id ao retorno do Update Produto

--- SenacSp.ProjetoIntegrador.Domain/CommandHandlers/ProductCommandHandler.cs
+++ SenacSp.ProjetoIntegrador.Domain/CommandHandlers/ProductCommandHandler.cs
@@ -24,8 +24,8 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
         IRequestHandler<ChangeStockQuantityCommand, DefaultResult>,
         IRequestHandler<AddQuestionsAndAnswerProductCommand, DefaultResult>,
         IRequestHandler<InsertProductImageCommand, SaveImageResult>,
-        IRequestHandler<DeleteProductImageCommand,DefaultResult>,
-        IRequestHandler<ChangeProductStatusCommand,DefaultResult>
+        IRequestHandler<DeleteProductImageCommand, DefaultResult>,
+        IRequestHandler<ChangeProductStatusCommand, DefaultResult>
 
     {
         private readonly IProductRepository _productRepository;
@@ -33,6 +33,7 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
         private readonly IAwsS3StorageService _awsS3StorageService;
         private readonly AwsS3Config _awsS3Config;
         private readonly IProductImageRepository _productImageRepository;
+
         public ProductCommandHandler(IUnitOfWork uow, IDomainNotification notifications,
             IProductRepository productRepository,
             IProductQuestionAnswerRepository productQuestionAnswerRepository,
@@ -56,24 +57,24 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
                 Notifications.Handle("Produto Com esse nome já Cadastrado");
                 return null;
             }
+
             var product = Product.New(command.Name, command.Description, command.Price, command.Quantity);
 
             result.ProductId = product.Id;
 
             foreach (Guid keyWordId in command.KeyWords)
             {
-                product.KeyWords.Add(ProductKeyWord.New(product.Id,keyWordId));
+                product.KeyWords.Add(ProductKeyWord.New(product.Id, keyWordId));
             }
 
-             await _productRepository.AddAsync(product);
+            await _productRepository.AddAsync(product);
 
             if (!await CommitAsync())
             {
                 return result;
             }
+
             return result;
-
-
         }
 
         public async Task<CreateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
@@ -88,24 +89,29 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
                 return null;
             }
 
-            if (command.Name != product.Name && await _productRepository.AnyAsync(x => x.Name.ToLower().Equals(command.Name.ToLower())))
+            if (command.Name != product.Name &&
+                await _productRepository.AnyAsync(x => x.Name.ToLower().Equals(command.Name.ToLower())))
             {
                 Notifications.Handle("Não é possivel alterar o Nome para o de um produto que já existe");
                 return null;
             }
+
             product.Update(command.Name, command.Description, command.Price, command.Quantity);
             if (command.KeyWords != null)
             {
                 product.SetKeyWords(command.KeyWords);
             }
+
+            result.ProductId = product.Id;
+
             _productRepository.Modify(product);
 
             if (!await CommitAsync())
             {
                 return result;
             }
-            return result;
 
+            return result;
         }
 
         public async Task<DefaultResult> Handle(ChangeStockQuantityCommand command, CancellationToken cancellationToken)
@@ -119,6 +125,7 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
                 Notifications.Handle("Não foi possivel encontrar Produto");
                 return null;
             }
+
             product.ChangeStatus();
 
             _productRepository.Modify(product);
@@ -127,15 +134,17 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
             {
                 return result;
             }
+
             return result;
         }
 
-        public async Task<DefaultResult> Handle(AddQuestionsAndAnswerProductCommand command, CancellationToken cancellationToken)
+        public async Task<DefaultResult> Handle(AddQuestionsAndAnswerProductCommand command,
+            CancellationToken cancellationToken)
         {
             var result = new DefaultResult();
 
             var product = await _productRepository.FindAsync(x => x.Id == command.Id);
-            
+
             if (product == null)
             {
                 Notifications.Handle("Não foi possivel encontrar Produto");
@@ -148,45 +157,53 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
             {
                 questionsAnswers.Add(ProductQA.New(command.Id, questionAnswer));
             }
+
             await _productQuestionAnswerRepository.AddRangeAsync(questionsAnswers);
 
             if (!await CommitAsync())
             {
                 return result;
             }
+
             return result;
         }
 
-        public async Task<SaveImageResult> Handle(InsertProductImageCommand command, CancellationToken cancellationToken)
+        public async Task<SaveImageResult> Handle(InsertProductImageCommand command,
+            CancellationToken cancellationToken)
         {
             var result = new SaveImageResult();
-            
+
             var product = await _productRepository.FindAsync(x => x.Id == command.ProductId);
             if (product == null)
             {
                 Notifications.Handle("Produto Não encotrado");
                 return null;
             }
+
             foreach (var file in command.Files)
             {
-
                 var s3FilefullPath = _awsS3Config.BuildProductsFullPath(command.ProductId, Guid.NewGuid().ToString());
 
 
-                var storedFile = await _awsS3StorageService.StoreFile(file.Buffer, _awsS3Config.BuildProductsS3Path(command.ProductId, Guid.NewGuid().ToString()), true);
-               
+                var storedFile = await _awsS3StorageService.StoreFile(file.Buffer,
+                    _awsS3Config.BuildProductsS3Path(command.ProductId, Guid.NewGuid().ToString()), true);
+
                 if (storedFile.IsNull())
                 {
                     Notifications.Handle("Erro ao salvar Imagem");
                     return null;
                 }
+
                 _productImageRepository.Add(ProductImage.New(product.Id, storedFile));
                 result.Links.Add(storedFile);
-            };
+            }
+
+            ;
             if (!await CommitAsync())
             {
                 return result;
             }
+
             return result;
         }
 
@@ -201,31 +218,34 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
                 Notifications.Handle("Imagem de produto não encontrado");
                 return null;
             }
+
             if (!(await _awsS3StorageService.DeleteFile(productImage.ImagePath)))
             {
                 Notifications.Handle("Não foi possivel deletar a imagem");
                 return null;
             }
-                _productImageRepository.Remove(productImage);
+
+            _productImageRepository.Remove(productImage);
 
             if (!await CommitAsync())
             {
                 return result;
             }
-            return result;
 
+            return result;
         }
 
         public async Task<DefaultResult> Handle(ChangeProductStatusCommand command, CancellationToken cancellationToken)
         {
             var result = new DefaultResult();
-            
+
             var product = await _productRepository.FindAsync(x => x.Id == command.Id);
             if (product == null)
             {
                 Notifications.Handle("Produto não encontrado");
                 return null;
             }
+
             product.ChangeStatus();
 
             _productRepository.Modify(product);
@@ -235,6 +255,7 @@ namespace SenacSp.ProjetoIntegrador.Domain.CommandHandlers
             {
                 return null;
             }
+
             return result;
         }
     }
